5137cf510af36a2eac42ad41c9fa0c4b0a301009
Taewoong <aipooh8882@naver.com>
2021-10-22 14:35:39 +0900

    refactored the middleware

--- back/routes/middlewares.js
+++ back/routes/middlewares.js
@@ -1,27 +1,30 @@
 const jwt = require('jsonwebtoken');
 
-exports.verifyToken = (req, res, next) => {
-    try{
-        const token = req.headers.cookie.slice(4);
-        req.decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.data = req.decoded.id;
-        return next();
-    }
-    catch(err){
-        //유효기간 초과
-        if(err.name === 'TokenExpiredError'){
-            res.redirect('/?error=Token expiration');
-        }
-        res.redirect('/?error=Invalied token');
-    }
-};
-
 exports.isLoggedIn = (req, res, next) => {
     if(req.isAuthenticated()){
         next();
     }
     else{
         res.status(403).send('you need to login');
+    }
+};
+
+exports.verifyToken = (req, res, next) => {
+    try{
+        req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        req.data.decoded;
+        return next();
+    } catch(err){
+        if(err.name === 'TokenExpiredError') { //유효기간 초과
+            return res.status(419).json({
+                code: 419,
+                message: 'token has been expired',
+            });
+        }
+        return res.status(401).json({
+            code: 401,
+            message: 'unvalid token',
+        });
     }
 };
 
